a68c1bf6b2c81e10602ae98d218e82316771a22a
hkalinouski <7742589@gmail.com>
2017-05-08 17:32:57 +0300

    todo template

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,29 @@
 const router = require('koa-router')()
-var views = require('koa-views')
+const views = require('koa-views')
+const bodyParser = require('koa-bodyparser')
+let data = [{task: 'get milk'},{task: 'walk dog8'}, {task: 'some coding'}]
 
 module.exports = function(app) {
   app.use(views(`views`, { extension: 'ejs' }))
 
   router.get('/todo', async (ctx) => {
-    await ctx.render('todo')
+    ctx.status = 200
+    await ctx.render('todo', {todos: data})
   })
 
-  router.post('/todo', (ctx) => {
-    console.log('post')
+  router.post('/todo', async (ctx) => {
+    data.push(ctx.request.body)
+    // await ctx.render('todo', {todos: data})
+    ctx.status = 201
+    ctx.res.setHeader('Content-Type', 'application/json')
+    ctx.body = data
   })
-
 
   router.delete('/todo', (ctx) => {
     console.log('delete')
   })
 
-
-
+  app.use(bodyParser());
   app.use(router.routes());
 }
 
